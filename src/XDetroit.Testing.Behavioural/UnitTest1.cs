cf9ee01f7bcfa32b31c9200d94bf687c6f431c87
Rich <ric.fra@hotmail.it>
2018-06-25 12:18:48 +0100

    Adds behavioural testing project.

---
+++ src/XDetroit.Testing.Behavioural/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XDetroit.Testing.Behavioural
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
