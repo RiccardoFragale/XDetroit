c58a1b7a315e66806eb52ffc34e8148b119eb4f8
Rich <ric.fra@hotmail.it>
2018-06-25 16:30:15 +0100

    Implements get method in Entity framework double class.

--- src/XDetroit.WebFrontend/Dal/DataProvider.cs
+++ src/XDetroit.WebFrontend/Dal/DataProvider.cs
@@ -1,12 +1,25 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace XDetroit.WebFrontend.Dal
 {
     public class DataProvider : IDataProvider
     {
+        private Dictionary<Type, List<object>> entities = new Dictionary<Type, List<object>>();
+        
         public IQueryable<T> GetEntities<T>() where T : class
         {
-            throw new System.NotImplementedException();
+            var type = typeof(T);
+            var returnValue = Enumerable.Empty<T>();
+            if (entities.ContainsKey(type))
+            {
+                var list = entities[type];
+                IEnumerable<T> entityCollection = list.Cast<T>();
+                returnValue = entityCollection;
+            }
+
+            return returnValue.AsQueryable();
         }
     }
 }
